af1efbf0970bb1b19d8673dcf3ff6b3bccebff38
carlosmore1 <carlos_more@usmp.pe>
2025-08-22 13:47:42 -0500

    Agregar vistas para mascota

--- obj/Debug/net9.0/Veterinaria.AssemblyInfo.cs
+++ obj/Debug/net9.0/Veterinaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d6bc4e4e18122c36b20a8a04ac617807257020")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9bc2e44d541239d43f58aab68c79dda56988188")]
 [assembly: System.Reflection.AssemblyProductAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
