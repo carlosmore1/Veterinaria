18cd41a9a0060a8a55dceaf58fe62a119a777e47
carlosmore1 <carlos_more@usmp.pe>
2025-08-22 14:28:33 -0500

    ulti

--- obj/Debug/net9.0/Veterinaria.AssemblyInfo.cs
+++ obj/Debug/net9.0/Veterinaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393d4c118196a56a33d6388b245fecd80c147906")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca9ab142291d8741ff0d510ae3d4acbc663f155")]
 [assembly: System.Reflection.AssemblyProductAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
